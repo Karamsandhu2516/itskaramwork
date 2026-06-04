edca65772fc54cc3c77abe1a173e2badf8654443
Karamjit Kaur <karamsandhu2516@gmail.com>
2026-06-04 19:52:04 -0400

    feat: add 3-tier-Architecture to projects and sort list

--- src/data/icons.ts
+++ src/data/icons.ts
@@ -6,6 +6,7 @@ import PosthogIcon from '../assets/logos/posthog-logo.svg?raw';
 import PythonIcon from '../assets/logos/Python-logo.svg?raw';
 import TailwindIcon from '../assets/logos/Tailwind-logo.svg?raw';
 import dockerIcon from '../assets/logos/docker-logo.svg?raw';
+import TerraformIcon from '../assets/logos/terraform-logo.svg?raw';
 
 export {
     PhoenixIcon,
@@ -14,5 +15,7 @@ export {
     PosthogIcon,
     PythonIcon,
     TailwindIcon,
-    dockerIcon
+    dockerIcon,
+    TerraformIcon
 };
+
